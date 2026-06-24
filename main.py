b459941a92c3efbbd952a1ddb409d90883118648
aicareeros-dotcom <aicareeros@gmail.com>
2026-06-25 00:33:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+import asyncio
+
+try:
+    asyncio.get_event_loop()
+except RuntimeError:
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+from pyrogram import Client, filters, idle
 # 🔧 Standard Library
 import os
 import re
